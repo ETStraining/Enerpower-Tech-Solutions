299a3da5a2c4e4e31d7af16101c424609d3327b8
UAlice1 <umubyeyialice7@gmail.com>
2024-10-07 17:55:23 +0100

    last commit

--- app/component/boarding.tsx
+++ app/component/boarding.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Boarding: React.FC = () => {
   return (
-    <div className="relative flex flex-col items-end sm:items-center"> 
-      
-      {/* Background image wrapper */}
-      <div className="relative w-full sm:w-3/4 md:w-1/2 lg:w-3/3 mb-8 flex justify-center sm:justify-center">
-        {/* Background image */}
-        <div className="absolute inset-0 bg-cover bg-center rounded-lg shadow-lg opacity-50"
-             style={{ backgroundImage: "url('/path-to-background-image.jpg')" }}></div>
+    <>
+      <div className="relative flex items-end sm:items-center">
         
-        {/* Foreground image */}
-        <img 
-          src="/Rectangle 26.png" 
-          alt="Description of the picture" 
-          className="relative rounded-lg shadow-lg w-full"
-        />
+      <div
+  className="relative flex items-center justify-center w-full sm:w-4/5 md:w-3/4 lg:w-2/3 bg-cover bg-center p-6 rounded-lg"
+  style={{ backgroundImage: "url('/est.png')" }}
+>
+  <div className="p-4 rounded-lg text-center w-full">
+    <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl mb-4 font-bold">
+      Technology
+      <br /> Design,
+      <br /> Build & <br />
+      Customer <br /> Experience <br /> Experts.
+    </h2>
+  </div>
+</div>
+
+
+<div className="relative w-full max-w-full sm:max-w-4/5 md:max-w-3/4 lg:max-w-2/3 mb-8 flex justify-center">
+  <img
+    src="/Rectangle 26.png"
+    alt="Description of the picture"
+    className="rounded-lg shadow-md opacity-80 w-full h-auto"
+  />
+</div>
+
       </div>
 
-      {/* Text block with space added */}
-      <div className="absolute top-1/3 left-1/4 sm:top-1/3 sm:right-1/3 transform -translate-y-1/2 bg-white bg-opacity-75 p-4 rounded-lg shadow-lg text-center mt-4"> 
-        <h2 className="text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl mb-4">
-          TECHNOLOGY<br />
-          DESIGN BUILD & <br />
-          CUSTOMER EXPERIENCE EXPERT
-        </h2>
-
-        {/* Button added below the text */}
-        <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">
-          Learn More
+      <div className="flex justify">
+        <button
+          className="flex items-center bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-700 ml-16 transition duration-300 ease-in-out mb-2"
+          style={{ height: '36px', width: '140px', fontSize: '12px' }} 
+        >
+          <span className=" text-xs ml-6">Build a Team</span> 
+          <FontAwesomeIcon icon={faArrowRight} className="text-2xl" style={{height:'10px',width:'20px'}} /> 
         </button>
       </div>
-      
-    </div>
+    </>
   );
 };
 
