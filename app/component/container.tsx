9c20d3dbfdb9e8d84c097e648f866de8d986cc2d
aimee-mercury <ingabiremarieaimee6@gmail.com>
2024-10-08 17:58:34 +0000

    changes

--- app/component/container.tsx
+++ app/component/container.tsx
@@ -1,10 +1,9 @@
 // Container.tsx
 "use client";
 import React, { FC } from "react";
-import { useTheme } from '../component/themeContent'; // Adjust the path as needed
 
 const Container: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { isDarkMode } = useTheme();
+
 
   return (
     <div className={`container mx-auto px-4`}>
