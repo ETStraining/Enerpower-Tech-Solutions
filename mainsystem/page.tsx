83896c638db02e52c35ced8535586d25cf706d3f
Mlouise99 <mwubahalouise99@gmail.com>
2024-10-24 18:20:58 +0200

    Doing main system

---
+++ mainsystem/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Footer from '../components/footer';
+
+const Mainsystem = () => {
+  return (
+    <div className="container mx-auto  py-6">
+    
+      <div className="relative w-full  ">
+        <Image
+          src="/main.png"
+          alt="Main System Equipment"
+          layout="responsive"
+          width={500} 
+          height={300} 
+         
+         
+        />
+        
+        <div className="absolute inset-0 flex flex-col items-center justify-center mb-80 text-white   z-10">
+          <h1 className="text-4xl sm:text-4xl font-bold">Main system</h1>
+          <p className="mt-4 text-lg sm:text-4xl font-medium">v Supply of materials, equipment, and tools</p>
+          <p className="text-lg font-medium sm:text-2xl" >v Installation Services</p>
+          <p className="text-lg font-medium sm:text-2xl">v Scheduled Preventive Maintenance and repair services</p>
+        </div>
+      </div>
+
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default Mainsystem;
