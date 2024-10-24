162126b7baf094e015599cc4dbd4a48b7b55d516
Mlouise99 <mwubahalouise99@gmail.com>
2024-10-24 18:31:28 +0200

    doing on main system

--- mainsystem/page.tsx
+++ mainsystem/page.tsx
@@ -18,7 +18,7 @@ const Mainsystem = () => {
         
         <div className="absolute inset-0 flex flex-col items-center justify-center mb-80 text-white   z-10">
           <h1 className="text-4xl sm:text-4xl font-bold">Main system</h1>
-          <p className="mt-4 text-lg sm:text-4xl font-medium">v Supply of materials, equipment, and tools</p>
+          <p className="mt-4 text-lg sm:text-4xl sm:text-2xl font-medium">v Supply of materials, equipment, and tools</p>
           <p className="text-lg font-medium sm:text-2xl" >v Installation Services</p>
           <p className="text-lg font-medium sm:text-2xl">v Scheduled Preventive Maintenance and repair services</p>
         </div>
