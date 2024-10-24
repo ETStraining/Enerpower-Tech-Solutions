1b9858f02915d5bb08d15c6842b8374d53cb9629
Mlouise99 <mwubahalouise99@gmail.com>
2024-10-24 19:41:19 +0200

    resoliving errors

--- app/mainsystem/page.tsx
+++ app/mainsystem/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import Footer from '@/components/footer';
+import Container from '@/components/container';
 
 const Mainsystem = () => {
   return (
-    <div className="container mx-auto py-6">
+<Container>
+    <div className="container mx-auto py-16">
       <div className="relative w-full">
       
         <Image
@@ -16,17 +18,18 @@ const Mainsystem = () => {
         />
 
         
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-start mt-72  text-white z-10 px-4">
-          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Main System</h1>
-          <p className="mt-2 text-sm sm:text-lg md:text-xl font-medium">.Supply of materials, equipment, and tools</p>
-          <p className="text-sm sm:text-lg md:text-xl font-medium">     . Installation Services</p>
-          <p className="text-sm sm:text-lg md:text-xl font-medium">     . Scheduled Preventive Maintenance and repair services</p>
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-start mt-56  text-white z-10 px-4">
+          <h1 className="text-2xl sm:text-5xl md:text-6xl font-bold mb-4">Main System</h1>
+          <p className="mt-2 text-sm sm:text-lg md:text-xl font-medium">v Supply of materials, equipment, and tools</p>
+          <p className="text-sm sm:text-lg md:text-xl font-medium">     v Installation Services</p>
+          <p className="text-sm sm:text-lg md:text-xl font-medium">     v Scheduled Preventive Maintenance and repair services</p>
         </div>
       </div>
 
      
       <Footer />
     </div>
+    </Container>
   );
 };
 
