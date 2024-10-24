8a15398093de8286f8b8c620907839222654af8a
UAlice1 <umubyeyialice7@gmail.com>
2024-10-24 11:13:55 +0100

    pulling

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Boarding from "@/components/sections/boarding";
+import About from "@/components/sections/about";
+import Mission from "@/components/sections/mission";
+import Footer from "@/components/footer";
+
+
+export default function Home() {
+  return (
+    <div>
+        <Boarding />
+        <About />
+        <Mission/>
+        <Footer />
+    </div>
+  );
+}
