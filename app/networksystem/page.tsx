7ffc21f1cfe2b961bddd889ace6a53563cdce23a
Marie Rose <marieroseuwizeyimana440@gmail.com>
2024-10-25 13:07:12 +0200

    Networkingsystemsecond

--- app/networksystem/page.tsx
+++ app/networksystem/page.tsx
@@ -5,23 +5,23 @@ import Container from '@/components/container'
 const NetworkSystem = () => {
   return (
     <div>
-     
+     <Container>
       <div
-        className="relative h-[400px] md:h-[500px] lg:h-[600px] bg-cover bg-center flex justify-center items-center"
+        className="relative h-[400px] md:h-[500px] lg:h-[400px] bg-cover bg-center border-radius:10px flex justify-center items-center"
         style={{
           backgroundImage: 'url("/netsystem.png")',
         }}
       >
         <div className="absolute inset-0 bg-black opacity-40"></div>
-        <div className="relative text-center text-white">
-          <h1 className="text-3xl md:text-5xl font-bold mb-2 mt-28 drop-shadow-md">Networking and Installation <br />Services</h1>
-          <p className="text-lg md:text-2xl drop-shadow-sm">our Networking and Installation <br /> department is dedicated to providing <br />cutting-edge tecnology solutions to <br /> meet your business needs</p>
+        <div className="relative text-center  text-white">
+          <h1 className="text-3xl md:text-3xl font-bold mb-2 mt-28 drop-shadow-md">Networking and Installation <br />Services</h1>
+          <p className="text-lg md:text-xl ">our Networking and Installation <br /> department is dedicated to providing <br />cutting-edge tecnology solutions to <br /> meet your business needs</p>
         </div>
       </div>
-     <Container>
-     <div className="space-y-6 text-left">
-          <h2 className="text-2xl font-bold mb-4">Our expertise includes:</h2>
-          <ul className="list-disc pl-5 space-y-4">
+     
+     <div className="space-y-6 text-left mt-4">
+          <h2 className="text-2xl font-bold mb-10">Our expertise includes:</h2>
+          <ul className="list-disc pl-5 space-y-8 ">
             <li>
               <strong>Computers, Laptops, and Desktops:</strong> Supplying, configuring, and installing high-performance computing systems to enhance productivity.
             </li>
@@ -36,7 +36,7 @@ const NetworkSystem = () => {
             </li>
           </ul>
           
-          <p className="">
+          <p className="mb-8">
             Our comprehensive networking solutions ensure that your business operates smoothly with secure and reliable IT infrastructure.
           </p>
         
