324b1681191d4a73941ecb3966b0caec46b7e3fd
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2024-10-28 16:55:28 +0200

    new fixes on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,5 @@
 "use client";
 import Image from "next/image";
-import { IoMdCall } from "react-icons/io";
-import { MdOutlineEmail } from "react-icons/md";
-import { IoLocationOutline } from "react-icons/io5";
 import { CiLinkedin, CiFacebook } from "react-icons/ci";
 import { FaInstagram } from "react-icons/fa6";
 import { LiaTwitterSquare } from "react-icons/lia";
@@ -14,17 +11,17 @@ import { useState, useEffect } from "react";
 export default function Footer() {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         const savedTheme = localStorage.getItem("darkMode")
         if (savedTheme) {
             const darkModeEnabled = JSON.parse(savedTheme);
             setIsDarkMode(darkModeEnabled);
             document.body.classList.toggle('dark-mode', darkModeEnabled);
             document.body.classList.toggle('light-mode', !darkModeEnabled);
-          } else {
-            document.body.classList.add('light-mode'); 
-            
-          }
+        } else {
+            document.body.classList.add('light-mode');
+
+        }
     }, [])
     return (
         <Container>
@@ -35,68 +32,63 @@ export default function Footer() {
                         alt="logo"
                         width={100}
                         height={100}
-                        className="my-7"
+                        className="w-[12rem] h-[18rem]"
                     />
                     <p className="pb-7">
                         Expertly trained, battle-tested,
-                        <br />elite software developers on <br />demand
+                        elite<br /> software developers on demand
                     </p>
                     <div className="flex gap-4 items-end pb-7">
-                        <IoMdCall fontSize={30} />
-                        <p>+94 713249222</p>
+                        Tel +250 788 781 418
                     </div>
 
                     <div className="flex gap-4 items-end pb-7">
-                        <MdOutlineEmail fontSize={25} />
-                        <p>hello@courtney.lk</p>
+                        enerpowertechsolutionsltd@gmail.com
                     </div>
 
-                    <div className="flex gap-4 items-center pb-7">
-                        <IoLocationOutline fontSize={30} />
-                        <p>
-                            Courtney Lanka (Private) Limited <br />
-                            Level 35 <br />World Trade Center <br />Colombo 01
-                        </p>
-                    </div>
-
-                    <h3 className="pb-4">FOLLOW US</h3>
+                    <h3 className="pb-4">Follow us</h3>
                     <div className="flex gap-3 pt-3">
-                    <a href="https://www.linkedin.com/company/enerpower-tech-solutions/" target="_blank" >
-                        <CiLinkedin fontSize={35} color={isDarkMode ? 'white' : 'black'} />
-                    </a>
-                    <a href="#">
-                        <CiFacebook fontSize={35} color={isDarkMode ? 'white' : 'black'} />
-                    </a>
-                    <a href="#">
-                        <FaInstagram fontSize={35} color={isDarkMode ? 'white' : 'black'} />
-                    </a>
-                    <a href="#">
-                        <LiaTwitterSquare fontSize={35} color={isDarkMode ? 'white' : 'black'} />
-                    </a>
-                    <a href="#">
-                        <CgYoutube fontSize={35} color={isDarkMode ? 'white' : 'black'} />
-                    </a>
-                </div>
+                        <a href="https://www.linkedin.com/company/enerpower-tech-solutions/" target="_blank" >
+                            <CiLinkedin fontSize={35} color={isDarkMode ? 'white' : 'black'} />
+                        </a>
+                        <a href="#">
+                            <CiFacebook fontSize={35} color={isDarkMode ? 'white' : 'black'} />
+                        </a>
+                        <a href="#">
+                            <FaInstagram fontSize={35} color={isDarkMode ? 'white' : 'black'} />
+                        </a>
+                        <a href="#">
+                            <LiaTwitterSquare fontSize={35} color={isDarkMode ? 'white' : 'black'} />
+                        </a>
+                        <a href="#">
+                            <CgYoutube fontSize={35} color={isDarkMode ? 'white' : 'black'} />
+                        </a>
+                    </div>
                 </div>
 
-                <div className="flex flex-col gap-5 mt-20">
-                    <h2 className="font-semibold">Company</h2>
+                <div className="flex flex-col gap-7 mt-20">
+                    <h2 className="font-semibold pb-5">Company</h2>
                     <Link href="/public">About us</Link>
-                    <Link href="/public">Success Stories</Link>
                     <Link href="/public">Privacy policy</Link>
                     <Link href="/public">Terms & consitions</Link>
                     <Link href="/public">Contact us</Link>
                 </div>
 
-                <div className="flex flex-col gap-5 mt-20">
-                    <h2 className="font-semibold">Services</h2>
-                    <p>Hire permanent staff</p>
-                    <p>Staff Augumentation</p>
-                    <p>Software outsourcing</p>
-                    <p>Build remote office</p>
+                <div className="flex flex-col gap-7 mt-20">
+                    <h2 className="font-semibold pb-5">Services</h2>
+                    <p>Main Services</p>
+                    <p>Electrical System</p>
+                    <p>Networking System</p>
+                    <p>Security System</p>
                 </div>
 
-               
+                <div className="flex flex-col gap-7 mt-20">
+                    <h2 className="font-semibold pb-5">How to Start</h2>
+                    <p>You asked</p>
+                    <p>We proceed</p>
+                    <p>Negotiate</p>
+                    <p>You get</p>
+                </div>
             </div>
         </Container>
     );
